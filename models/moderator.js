24d3e2dafbbb1dea72173b0c7f31a6277645a0cf
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-09 14:38:51 -0400

    Directory architecture changed. JAWS database configured. Moderator model added. Irrelevant files deleted, i.e. canary.test.js. Views scheme updated.

---
+++ models/moderator.js
@@ -0,0 +1,39 @@
+module.exports = function(sequelize, DataTypes) {
+  var Moderator = sequelize.define("Moderator", {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        len: [1,100],
+      }
+    },
+
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        len: [1,100],
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8,100]
+      }
+    },
+
+    email: {
+      type: DataTypes:STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    }
+
+  });
+  return Moderator;
+};
